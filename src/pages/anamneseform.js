ec03b23d44b30c552e95a7d4450cabdc856a6466
jcgsr <cabralguerra@gmail.com>
2021-07-28 19:45:10 -0300

    form anamnese

--- src/pages/anamneseform.js
+++ src/pages/anamneseform.js
@@ -137,71 +137,72 @@ const Anamnese = () => {
         <div className="checkboxes" style={{ marginTop: "1rem" }}>
           <div onChange={e => setAlergias(e.target.value)}>
             <p>1. Alergias?</p>
-            <label htmlFor="alergias">Sim</label>
             <input type="radio" name="alergias" value="sim" />
-            <label htmlFor="alergias">Não</label>
+            <label htmlFor="alergias">Sim</label>
+            <br />
             <input type="radio" name="alergias" value="não" />
+            <label htmlFor="alergias">Não</label>
           </div>
           <div onChange={e => setHipertensao(e.target.value)}>
             <p>2. Hipertensão?</p>
             <input type="radio" name="hipertensao" value="sim" />
-            <label htmlFor="hipertensao">Sim</label>
+            <label htmlFor="hipertensao">Sim</label> <br />
             <input type="radio" name="hipertensao" value="não" />
             <label htmlFor="hipertensao">Não</label>
           </div>
           <div onChange={e => setDiabetes(e.target.value)}>
             <p>3.Diabetes?</p>
             <input type="radio" name="diabetes" value="sim" />
-            <label htmlFor="diabetes">Sim</label>
+            <label htmlFor="diabetes">Sim</label> <br />
             <input type="radio" name="diabetes" value="não" />
             <label htmlFor="diabetes">Não</label>
           </div>
           <div onChange={e => setMarcapasso(e.target.value)}>
             <p>4. Marcapasso?</p>
             <input type="radio" name="marcapasso" value="sim" />
-            <label htmlFor="marcapasso">Sim</label>
+            <label htmlFor="marcapasso">Sim</label> <br />
             <input type="radio" name="marcapasso" value="não" />
             <label htmlFor="marcapasso">Não</label>
           </div>
           <div onChange={e => setPinos(e.target.value)}>
             <p>5. Pinos ou placas?</p>
             <input type="radio" name="pinos" value="sim" />
-            <label htmlFor="pinos">Sim</label>
+            <label htmlFor="pinos">Sim</label> <br />
             <input type="radio" name="pinos" value="não" />
             <label htmlFor="pinos">Não</label>
           </div>
           <div onChange={e => setHernia(e.target.value)}>
             <p>6. Hérnia?</p>
             <input type="radio" name="hernia" value="sim" />
-            <label htmlFor="hernia">Sim</label>
+            <label htmlFor="hernia">Sim</label> <br />
             <input type="radio" name="hernia" value="não" />
             <label htmlFor="hernia">Não</label>
           </div>
           <div onChange={e => setTrombose(e.target.value)}>
             <p>7. Trombose?</p>
             <input type="radio" name="trombose" value="sim" />
-            <label htmlFor="trombose">Sim</label>
+            <label htmlFor="trombose">Sim</label> <br />
             <input type="radio" name="trombose" value="não" />
             <label htmlFor="trombose">Não</label>
           </div>
           <div onChange={e => setCancer(e.target.value)}>
             <p>8. Câncer?</p>
             <input type="radio" name="cancer" value="sim" />
-            <label htmlFor="cancer">Sim</label>
+            <label htmlFor="cancer">Sim</label> <br />
             <input type="radio" name="cancer" value="não" />
             <label htmlFor="cancer">Não</label>
           </div>
           <div onChange={e => setHIV(e.target.value)}>
             <p>9. HIV?</p>
             <input type="radio" name="hiv" value="sim" />
-            <label htmlFor="hiv">Sim</label>
+            <label htmlFor="hiv">Sim</label> <br />
             <input type="radio" name="hiv" value="não" />
             <label htmlFor="hiv">Não</label>
           </div>
           <div onChange={e => setGravidez(e.target.value)}>
             <p>10. Gravidez?</p>
             <input type="radio" name="gravidez" value="sim" />
-            <label htmlFor="gravidez">Sim</label>
+            <label htmlFor="gravidez">Sim</label> <br />
             <input type="radio" name="gravidez" value="não" />
             <label htmlFor="gravidez">Não</label>
             <div onChange={e => setMeses(e.target.value)}>
@@ -212,28 +213,28 @@ const Anamnese = () => {
           <div onChange={e => setGripe(e.target.value)}>
             <p>11. Gripe ou resfriado?</p>
             <input type="radio" name="gripe" value="sim" />
-            <label htmlFor="gripe">Sim</label>
+            <label htmlFor="gripe">Sim</label> <br />
             <input type="radio" name="gripe" value="não" />
             <label htmlFor="gripe">Não</label>
           </div>
           <div onChange={e => setDepressao(e.target.value)}>
             <p>12. Depressão?</p>
             <input type="radio" name="depressao" value="sim" />
-            <label htmlFor="depressao">Sim</label>
+            <label htmlFor="depressao">Sim</label> <br />
             <input type="radio" name="depressao" value="não" />
             <label htmlFor="depressao">Não</label>
           </div>
           <div onChange={e => setAnsiedade(e.target.value)}>
             <p>13. Ansiedade?</p>
             <input type="radio" name="ansiedade" value="sim" />
-            <label htmlFor="ansiedade">Sim</label>
+            <label htmlFor="ansiedade">Sim</label> <br />
             <input type="radio" name="ansiedade" value="não" />
             <label htmlFor="ansiedade">Não</label>
           </div>
           <div onChange={e => setDores(e.target.value)}>
             <p>14. Dores?</p>
             <input type="radio" name="dores" value="sim" />
-            <label htmlFor="dores">Sim</label>
+            <label htmlFor="dores">Sim</label> <br />
             <input type="radio" name="dores" value="não" />
             <label htmlFor="dores">Não</label>
             <div onChange={e => setCorpo(e.target.value)}>
@@ -244,7 +245,7 @@ const Anamnese = () => {
           <div onChange={e => setOutraQuestao(e.target.value)}>
             <p>15. Alguma outra questão de saúde relevante para considerar?</p>
             <input type="radio" name="questão" value="sim" />
-            <label htmlFor="questão">Sim</label>
+            <label htmlFor="questão">Sim</label> <br />
             <input type="radio" name="questão" value="não" />
             <label htmlFor="questão">Não</label>
             <div onChange={e => setQual(e.target.value)}>
