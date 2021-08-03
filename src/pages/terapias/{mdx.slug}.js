08140791d59ba5183ebad4919c115e427ce09cf2
jcgsr <cabralguerra@gmail.com>
2021-08-03 12:34:50 -0300

    mdx bug

--- src/pages/terapias/{mdx.slug}.js
+++ src/pages/terapias/{mdx.slug}.js
@@ -3,19 +3,11 @@ import Layout from "../../components/Layout.js";
 
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
-import { StaticImage } from "gatsby-plugin-image";
 
 const Massagens = ({ data }) => {
 	return (
 		<Layout>
 			<h1>{data.mdx.frontmatter.title}</h1>
-			<StaticImage
-				id={data.mdx.id}
-				src="../assets/images/susi.jpg"
-				alt="Foto de Reiki"
-				layout="constrained"
-				placeholder="blurred"
-			/>
 			<div className="about-card">
 				<MDXRenderer>{data.mdx.body}</MDXRenderer>
 			</div>
