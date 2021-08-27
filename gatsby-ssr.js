2aa884105e9fe420fc4a03f1ef5347ee6ced6527
jcgsr <cabralguerra@gmail.com>
2021-08-27 10:16:18 -0300

    ssr bug

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,16 +1,16 @@
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/database";
-import React from "react";
-//const React = require("react");
-const HeadComponents = [
-  <script
-    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2008019372085379"
-    crossOrigin="anonymous"
-    async
-  />,
-];
+//import React from "react";
+////const React = require("react");
+//const HeadComponents = [
+//<script
+//src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2008019372085379"
+//crossOrigin="anonymous"
+//async
+///>,
+//];
 
-exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  setHeadComponents(HeadComponents);
-};
+//exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
+//setHeadComponents(HeadComponents);
+//};
