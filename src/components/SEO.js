c20dc9f6a2788b57793c19ad18784abfc036d86b
jcgsr <cabralguerra@gmail.com>
2022-04-23 17:30:40 -0300

    google search console

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -27,6 +27,10 @@ const SEO = ({ title, description, image, article }) => {
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
+      <meta
+        name="google-site-verification"
+        content="tqRK5nLmRB2nc_5h7q_TW34OAQPJH10ygcweoO1w9Mc"
+      />
       {seo.url && <meta property="og:url" content={seo.url} />}
       {seo.title && <meta property="og:title" content={seo.title} />}
       {seo.description && (
