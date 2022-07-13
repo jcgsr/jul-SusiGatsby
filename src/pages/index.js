8961a0753ed297db9a55b16e32d33ec0bcbe5a05
jcgsr <cabralguerra@gmail.com>
2022-07-13 14:27:49 -0300

    lotties

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,20 @@
 import React from "react";
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import Lottie from "react-lottie";
+import animationData from "../../src/assets/lotties/110375-bapcelebration.json";
 
 import { StaticImage } from "gatsby-plugin-image";
 
 const Home = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
   return (
     <Layout>
       <SEO title="Home" description="Susi Pessôa - Terapeuta Holística" />
@@ -27,12 +37,18 @@ const Home = () => {
         <div id="susi-img">
           <StaticImage
             id="img"
-            src="../assets/images/susi.jpg"
+            src="../assets/images/susi2.jpg"
             alt="Foto de Susi"
             layout="constrained"
             placeholder="blurred"
           />
         </div>
+        <Lottie
+          className="lotties"
+          options={defaultOptions}
+          height={200}
+          width={400}
+        />
       </main>
     </Layout>
   );
